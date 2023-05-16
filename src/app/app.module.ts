83f6a1033578ce76bf179d97b5530e5276ad4574
Ruslan Lopez Carro <scherzo_16@hotmail.com>
2023-05-16 06:09:34 -0600

    se agregan botones para agregar y eliimnar plantillas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import { SurveyTemplateDetailsComponent } from './components/survey/survey-templ
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatInputModule} from "@angular/material/input";
 import {MatSelectModule} from "@angular/material/select";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
@@ -27,7 +29,9 @@ import {MatSelectModule} from "@angular/material/select";
     HttpClientModule,
     BrowserAnimationsModule,
     MatInputModule,
-    MatSelectModule
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
